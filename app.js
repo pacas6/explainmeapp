75779fedc9020c80d8f1ce4d9e98e34a6e5239fc
pacas6 <wesleyaugusto@outlook.pt>
2022-04-29 00:40:18 +0100

    update path 2

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public'))); // usa para poder acess
 
 app.use('/', indexRouter);
 app.use('/api/fundos', fundosRouter);
-app.use('https://explainmeapp.herokuapp.com',usersRouter)
+app.use('explainmeapp.herokuapp.com',usersRouter)
 
 
 
