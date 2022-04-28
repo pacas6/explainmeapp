b9282f1058e995f8c3b99a96cb95aaa87d0c54a6
pacas6 <wesleyaugusto@outlook.pt>
2022-04-29 00:46:00 +0100

    is now

--- public/javascripts/usersCount.js
+++ public/javascripts/usersCount.js
@@ -8,7 +8,7 @@ async function showUsersCount(){
     try {
         
         let values = await $.ajax({
-            url: `explainmeapp.herokuapp.com/`,
+            url: `https://explainmeapp.herokuapp.com/`,
             method: 'GET',
             datatype: 'json',
         });
